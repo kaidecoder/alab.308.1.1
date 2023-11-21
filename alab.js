b37f52a9bd4d600d79ac0a19992a90f39b5d9b52
kaidecoder <131601358+kaidecoder@users.noreply.github.com>
2023-11-21 16:23:51 -0500

    compared the results

--- alab.js
+++ alab.js
@@ -1,5 +1,5 @@
 
-
+//Known Data
 let totalDistance = 1500
 let carSpeed1 = 55
 let carDistance1 = 30
@@ -10,31 +10,36 @@ let carDistance3 = 23
 let budget = 175
 let averageFuelCost = 3
 
-const time1 = carDistance1 / carSpeed1
-const time2 = carDistance2 / carSpeed2
-const time3 = carDistance3 / carSpeed3
-let totalTime = time1 + time2 + time2
-
-let gallons1 = carSpeed1 / carDistance1
-let gallons2 = carSpeed2 / carDistance2
-let gallons3 = carSpeed3 / carDistance3
+//Calculated Data
+let gallons1PerHour = carSpeed1 / carDistance1
+let gallons2PerHour = carSpeed2 / carDistance2
+let gallons3PerHour = carSpeed3 / carDistance3
 
 let averageSpeed  = (carSpeed1 + carSpeed2 + carSpeed3)/3
 
-totalTime = totalDistance / averageSpeed
+//Formula: Distance = Speed * Time
+let totalTime = totalDistance / averageSpeed
 
-let gals1 = gallons1 * totalTime
-let gals2 = gallons2 * totalTime
-let gals3 = gallons3 * totalTime
+//Find the gallons by speed travelled
+let gals1 = gallons1PerHour * totalTime
+let gals2 = gallons2PerHour * totalTime
+let gals3 = gallons3PerHour * totalTime
 
 let totalGallons = gals1 + gals2 + gals3
-console.log("The total number of gallons is: ", totalGallons)
+console.log("The total number of gallons is: ", Math.round(totalGallons) + " gallons.")
+
 let totalCost = averageFuelCost * totalGallons
 
 if(totalCost >= budget){
-    console.log("Your budget is enough to cover the fuel expense")
+    console.log("Your budget is enough to cover the fuel expense.")
 }else{
-    console.log("Sorry, you need more funds")
+    console.log("Sorry, you need more funds.")
 }
 
-console.log("The total time of the trip is: ", totalTime)
+console.log("The total time of the trip is: ", Math.round(totalTime) + " hours.")
+
+console.log("Comparing The Results")
+console.log("The gallons used at 55 miles per hour is: ", Math.round(gals1))
+console.log("The gallons used at 60 miles per hour is: ", Math.round(gals2))
+console.log("The gallons used at 75 miles per hour is: ", Math.round(gals3))
+console.log("It appears that the best rate of travel would be at 55 miles per hour.  However, the user would have to decide which is more important: saving on gas, or spending more time on the road.")
