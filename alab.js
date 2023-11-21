ab4c1e86c1b338d9feeef8771ca469704dd9e467
kaidecoder <131601358+kaidecoder@users.noreply.github.com>
2023-11-21 18:04:38 -0500

    updated my results

--- alab.js
+++ alab.js
@@ -1,45 +1,52 @@
 
 //Known Data
+// car speed in mph; car distance in mpg
 let totalDistance = 1500
 let carSpeed1 = 55
-let carDistance1 = 30
-let carSpeed2 = 60
-let carDistance2 = 28
+let rate1 = 30
+let carSpeed2 = 60 
+let rate2 = 28
 let carSpeed3 = 75
-let carDistance3 = 23
+let rate3 = 23
 let budget = 175
 let averageFuelCost = 3
 
 //Calculated Data
-let gallons1PerHour = carSpeed1 / carDistance1
-let gallons2PerHour = carSpeed2 / carDistance2
-let gallons3PerHour = carSpeed3 / carDistance3
+// totalDistance = averageSpeed * totalTime
 
-let averageSpeed  = (carSpeed1 + carSpeed2 + carSpeed3)/3
+// let time = totalDistance / speed
+let time1 = Math.round(totalDistance / carSpeed1)
+let time2 = Math.round(totalDistance / carSpeed2)
+let time3 = Math.round(totalDistance / carSpeed3)
+let totalTime = time1 + time2 + time3
 
-//Formula: Distance = Speed * Time
-let totalTime = totalDistance / averageSpeed
 
-//Find the gallons by speed travelled
-let gals1 = gallons1PerHour * totalTime
-let gals2 = gallons2PerHour * totalTime
-let gals3 = gallons3PerHour * totalTime
+//Find the gallons
+let gallons1 = totalDistance / rate1
+let gallons2 = totalDistance / rate2
+let gallons3 = totalDistance / rate3
 
-let totalGallons = gals1 + gals2 + gals3
-console.log("The total number of gallons is: ", Math.round(totalGallons) + " gallons.")
 
-let totalCost = averageFuelCost * totalGallons
+let totalGallons = gallons1 + gallons2 + gallons3
+console.log("The total number of gallons for the trip is: ", Math.round(totalGallons) + " gallons.")
 
-if(totalCost >= budget){
-    console.log("Your budget is enough to cover the fuel expense.")
+let totalCost = Math.round(averageFuelCost * totalGallons)
+
+if(totalCost <= budget){
+    console.log(`The total cost of fuel is $${totalCost}. Your budget of $${budget} is enough to cover the fuel expense.`)
 }else{
-    console.log("Sorry, you need more funds.")
+    console.log(`The total cost of fuel is $${totalCost}. Your budget of $${budget} is not enough.  Sorry, you need $${totalCost - budget} more.`)
 }
 
 console.log("The total time of the trip is: ", Math.round(totalTime) + " hours.")
 
 console.log("Comparing The Results")
-console.log("The gallons used at 55 miles per hour is: ", Math.round(gals1))
-console.log("The gallons used at 60 miles per hour is: ", Math.round(gals2))
-console.log("The gallons used at 75 miles per hour is: ", Math.round(gals3))
-console.log("It appears that the best rate of travel would be at 55 miles per hour.  However, the user would have to decide which is more important: saving on gas, or spending more time on the road.")
+console.log("The gallons used at" + " " + carSpeed1 +  " miles per hour is: ", Math.round(gallons1), " and the time it took is ", (time1)  + " hours.")
+
+console.log("The gallons used at" +  " " + carSpeed2 +  " miles per hour is: ", Math.round(gallons2), " and the time it took is ", (time2) + " hours.")
+
+console.log("The gallons used at" +  " " + carSpeed3 +  " miles per hour is: ", Math.round(gallons3), " and the time it took is ", (time3) + " hours.")
+
+
+
+console.log("The best rate of travel would have to be determined by the user.  It should be based on what is more valuable to the user - time or fuel.  If the user wants to save time above everything else, then she should travel at 75 miles per hour.  If she wants to save fuel instead, then she should travel at 55 miles per hour.")
